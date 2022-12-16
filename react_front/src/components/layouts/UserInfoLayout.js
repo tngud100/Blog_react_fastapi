cf795bde505e092b72daa0ea1ee564e830624c22
33285109@naver.com <33285109@naver.com>
2022-12-16 14:50:23 +0900

    컨트롤러 서비스

--- react_front/src/components/layouts/UserInfoLayout.js
+++ react_front/src/components/layouts/UserInfoLayout.js
@@ -1,12 +1,10 @@
 import React from "react";
 import PropTypes from "prop-types";
 import { Col, Container, Row } from "react-bootstrap";
-import MyNavbar from "components/commons/MyNavbar";
 
-const UserInfoLayout = ({ children, isNavbar }) => {
+const UserInfoLayout = ({ children }) => {
   return (
     <section style={{ backgroundColor: "#508bfc", minHeight: "100vh" }}>
-      {isNavbar ? <MyNavbar /> : null}
       <Container className="py-5 h-100">
         <Row className="d-flex justify-content-center align-items-center h-100">
           <Col className="col-12 col-md-8 col-lg-6 col-xl-5">{children}</Col>
